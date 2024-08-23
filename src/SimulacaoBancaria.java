bba747942241d14bbf9b308396c1e4bd649c2b2a
jotajot <josejoaomonteirocosta8@gmail.com>
2024-08-23 11:46:44 -0300

    subindo código

---
+++ src/SimulacaoBancaria.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class SimulacaoBancaria {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double saldo = 0;
+        boolean continuar = true;
+
+        while (continuar) {
+            System.out.println("Escolha uma opção:");
+            System.out.println("1 - Depositar");
+            System.out.println("2 - Sacar");
+            System.out.println("3 - Consultar Saldo");
+            System.out.println("0 - Sair");
+
+            int opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    // TODO: Ler o valor a ser depositado e atualizar/imprimir o saldo.
+                    System.out.println("Digite o valor para depósito:");
+                    double depositado = scanner.nextDouble();
+                    saldo += depositado;
+                    System.out.println("Saldo atual: " + saldo);
+                    break;
+                case 2:
+                    // TODO: Ler o valor a ser sacado e verificar/imprimir se há saldo suficiente.
+                    System.out.println("Digite o valor a ser sacado:");
+                    double sacado = scanner.nextDouble();
+                    saldo -= sacado;
+                    if(sacado<=saldo){
+                        System.out.println("Sacado com sucesso!.");
+                    } else if (sacado>saldo){
+                        System.out.println("Saldo insuficiente");
+                    }
+                    break;
+                case 3:
+                    // TODO: Exibir o saldo atual da conta.
+                    System.out.println("Saldo atual: " + saldo);
+                    break;
+                case 0:
+                    System.out.println("Programa encerrado.");
+                    continuar = false;  // Atualiza a variável de controle para encerrar o loop
+                    break;
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+            }
+        }
+        scanner.close();
+    }
+}
